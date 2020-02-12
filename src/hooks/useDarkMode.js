1c2085f2c3e7e73b627d856b4810289916533c4d
Amos Rose <amosroseacct@gmail.com>
2020-02-12 11:50:56 -0700

    step 3 complete and mvp reached

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import useLocalStorage from "./useLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = () => {
   const [value, setValue] = useLocalStorage("dark");
 
   useEffect(() => {
-    const body = document.querySelector(body);
+    const appBody = document.querySelector("body");
     if (value === true) {
-      body.classList.add("dark-mode");
+      appBody.classList.add("dark-mode");
     } else if (value === false) {
-      body.classList.remove("dark-mode");
+      appBody.classList.remove("dark-mode");
     }
   }, [value]);
 
